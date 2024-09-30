97f1ac9e3361017a33a135a0df32b9e026be6230
Pascal Otto Hetzel <hetzelp@cip.ifi.lmu.de>
2024-09-30 13:46:36 +0200

    added py scripts needed for ATACseq

---
+++ bin/filter_peaks.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import csv
+import argparse
+
+#> Argument Parser
+def parse_arguments():
+    # Create the argument parser
+    parser = argparse.ArgumentParser()
+
+    # Add an optional argument
+    parser.add_argument('-p', '--path', default="", help='path to folder', type=str)
+    parser.add_argument('-a', '--annotation', help='Filename of Annotations', type=str)
+    parser.add_argument('-i', '--input', help='Filename of Input bed file', type=str)
+    parser.add_argument('-o', '--output', help='Filename of filtered bed file', type=str)
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    return args
+
+#> Read in filtered annotations an write all PeakIDs into an array
+def get_PeakID(file_name):
+    PeakID = []
+    with open(file_name, 'r') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter='\t')
+        next(csvreader) # skip header row
+        for row in csvreader:
+            PeakID.append(row[0])
+    return PeakID
+
+#> Read in *.narrowPeak file and filter out all lines without valid annotation
+def filter_Peaks(file_name, PeakID):
+    peaks = []
+    with open(file_name, 'r') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter='\t')
+        for row in csvreader:
+            if(row[3] in PeakID):
+                peaks.append(row)
+    return peaks
+
+#> write new *.narrowPeak file
+def write_Peaks(file_name, peaks):
+    with open(file_name, 'w') as file:
+        for subarray in peaks:
+            for item in subarray:
+                file.write(str(item))
+                file.write('\t')
+            file.write('\n')
+            
+            
+#- main method        
+def main():
+    args = parse_arguments()
+    
+    annotation_file = args.path + args.annotation
+    peak_file = args.path + args.input
+    filtered_peaks = args.path +args.output
+    
+    PeakID = get_PeakID(annotation_file)
+    peaks = filter_Peaks(peak_file, PeakID)
+    write_Peaks(filtered_peaks, peaks)
+    print(len(peaks))
+
+if __name__ == '__main__':
+    main()
---
+++ bin/significant_genes.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+
+# Argument Parser
+def parse_arguments():
+    # Create the argument parser
+    parser = argparse.ArgumentParser()
+
+    # Add an optional argument
+    parser.add_argument('-p', '--path', default='',  help='path to folder', type=str)
+    parser.add_argument('-g', '--genes', help='Name or Path to gene list', default="peaks/annotation_genes_only.txt", type=str)
+    parser.add_argument('-a', '--annotation', help='Filename of Annotations', default="annotation_filtered.csv", type=str)
+    parser.add_argument('-c', '--count', help='threshold at which genes should be kept', default=10, type=int)
+    parser.add_argument('-o', '--outname', help='filename of output')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    return args
+
+#> Import all gene names
+#? read in all genes given in text file and return it as a dict; gene names := key
+def get_gene_names(filepath):
+    dict = {}                                       # create dictionary dict
+    
+    with open(filepath, 'r') as file:               # Open the file
+        lines = file.readlines()                    # and save each line in list
+
+    for line in lines:
+        line = line.strip()                         # strip whitespaces/new line chars
+        if line and line.startswith("FBgn"):        # If the line /= empty and is a gene, add it to the dictionary (init with 0 as count) and list
+            dict[line] = 0
+            
+    return dict
+
+#> Count all apperances of the genes in all annoations
+def count_genes(folder, annotation_filename, gene_count):
+    # Recursively walk through the folder and its subfolders
+    for root, dirs, files in os.walk(folder):
+        # Filter out all files that are not tsv-files
+        annotations = [file for file in files if file == annotation_filename]
+
+        # Iterate over all annotation-files in the current directory
+        for annotation in annotations:
+            file_path = os.path.join(root, annotation)
+
+            # extract the value of the 12th column of a tsv file?
+            # Open the annotation-file and read its content
+            with open(file_path, 'r') as f:
+                for line in f:
+                    line = line.strip()
+                    if line:
+                        values = line.split('\t')
+                        if len(values) >= 12:
+                            gene = values[11]
+                            if gene in gene_count:
+                                gene_count[gene] += 1
+    
+    return gene_count
+
+def filter_apperance(count, gene_count):
+    # Discard key and value from dictionary if value is smaller then value of variable "count"?
+    dict = {key: value for key, value in gene_count.items() if value >= count}
+    return dict
+            
+def write_genes(path, gene_count, outname):
+    # Write keys of dictionary to file?
+    file = open(path + outname, "w")
+    for key in gene_count.keys():
+        file.write(key +"\n") #+ "\t" + str(gene_count[key]) + "\n")
+    file.close()
+
+#! Main method
+def main():
+    args = parse_arguments()
+    gene_list_file = args.path + args.genes
+    annotation_filename = args.annotation
+    gene_counts = get_gene_names(gene_list_file)
+    gene_counts = count_genes(args.path, annotation_filename, gene_counts)
+    gene_counts = filter_apperance(args.count, gene_counts)
+    
+    write_genes(args.path, gene_counts, args.outname)
+    
+    print(len(gene_counts))
+    
+
+
+
+if __name__ == '__main__':
+    main()
